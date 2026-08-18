75c839cf02f9db5f968e94e8ab1745eaf0831ea3
hp967 <76082468+hp967@users.noreply.github.com>
2026-08-18 11:47:46 +0800

    Fix iOS version to 16

--- BionicDroneVisionServer/Package.swift
+++ BionicDroneVisionServer/Package.swift
@@ -1,50 +1,34 @@
-// swift-tools-version:5.7
+// swift-tools-version: 5.9
 import PackageDescription
 
 let package = Package(
     name: "BionicDroneVisionServer",
     platforms: [
-        .iOS(.v17)
-    ],
-    products: [
-        .library(
-            name: "BionicDroneVisionServer",
-            targets: ["BionicDroneVisionServer"]
-        ),
-    ],
-    dependencies: [
-        .package(url: "https://ghproxy.net/https://github.com/httpswift/swifter.git", from: "1.5.0"),
+        .iOS(.v16)
     ],
     targets: [
         .target(
             name: "BionicDroneVisionServer",
-            dependencies: [
-                .product(name: "Swifter", package: "swifter")
-            ],
             path: ".",
             sources: [
                 "BionicDroneVisionServerApp.swift",
                 "ContentView.swift",
-                "ModelManagerView.swift",
-                "ServerControlView.swift",
-                "TelemetryMonitorView.swift",
-                "TelemetryReceiver.swift",
-                "TelemetryModels.swift",
                 "APIModels.swift",
-                "NetworkHelper.swift",
                 "LlamaServer.swift",
-                "LlamaVisionEngine.swift"
+                "LlamaVisionEngine.swift",
+                "ModelManagerView.swift",
+                "NetworkHelper.swift",
+                "ServerControlView.swift",
+                "TelemetryModels.swift",
+                "TelemetryMonitorView.swift",
+                "TelemetryReceiver.swift"
             ],
             resources: [
-                .process("BionicDroneVisionServer-Bridging-Header.h")
+                .copy("Resources")
             ],
-            publicHeadersPath: "spm-headers",
-            cSettings: [
-                .headerSearchPath("../llama.cpp/include"),
-                .headerSearchPath("../llama.cpp/ggml/include"),
-                .headerSearchPath("../llama.cpp/src"),
-                .define("GGML_METAL"),
-                .define("GGML_OPENMP"),
+            linkerSettings: [
+                .unsafeFlags(["-framework", "Accelerate"]),
+                .unsafeFlags(["-F", "/usr/lib"]),
             ]
         )
     ]
