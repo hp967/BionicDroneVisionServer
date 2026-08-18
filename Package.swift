acbf539dff54b3a98cf7e6144c63559ae029c965
hp967 <76082468+hp967@users.noreply.github.com>
2026-08-18 22:37:37 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         .executableTarget(
             name: "BionicDroneVisionServer",
             dependencies: [],
-            path: "App"
+            path: "Sources"
         )
     ]
 )
